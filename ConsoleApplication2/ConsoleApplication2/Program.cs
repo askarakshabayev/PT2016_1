982ff4712867404843c7c7925208cee20b9bce72
askarakshabayev <askar.akshabayev@gmail.com>
2016-01-13 14:44:35 +0600

    added example of complex numbers

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -6,16 +6,42 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
+    class Complex
+    {
+        public int a, b;
+
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+        //public Complex add(Complex c)
+        //{
+        //    Complex result = new Complex(this.a + c.a, this.b + c.b);
+        //    return result;
+        //}
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(c1.a + c2.a, c1.b + c2.b);
+            return c3;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int a, b;
-            string s = Console.ReadLine();
-            string[] token = s.Split(); // 5 6 
-            a = int.Parse(token[0]);
-            b = int.Parse(token[1]);
-            Console.WriteLine(a + b);
+            Complex c = new Complex(5, 6);
+            Complex d = new Complex(5, 7);
+
+            Complex w = c + d + c + d + c + d;
+            Console.WriteLine(w);
             Console.ReadKey();
         }
     }
