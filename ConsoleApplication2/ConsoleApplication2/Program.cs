23b42d2131c2af1423d0bc90f70bc656812f18a8
askarakshabayev <askar.akshabayev@gmail.com>
2016-01-13 13:38:07 +0600

    changes

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.Write("Hello world");
+            Console.Write("testing commit");
             Console.ReadKey();
         }
     }
